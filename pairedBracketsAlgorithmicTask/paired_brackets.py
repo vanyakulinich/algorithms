63b3bd52c3096f3c2381989830b7828e6411488c
vanyakulinich <kulinichvanya@gmail.com>
2021-04-09 00:29:35 +0300

    refactor(python): python code formatted

--- pairedBracketsAlgorithmicTask/paired_brackets.py
+++ pairedBracketsAlgorithmicTask/paired_brackets.py
@@ -12,63 +12,64 @@ Task solution:
 This case is solved via one iteration ( O(n) ) with the help of stack data structure
 '''
 
+
 def validate_bracket_str(string=''):
-  '''
-    Validation of string with brackets. Returns false is string is empty.
-    Loops the string to check the brackets validation
+    '''
+      Validation of string with brackets. Returns false is string is empty.
+      Loops the string to check the brackets validation
 
-    :param string: string with brackets
-    :return: boolean validation result
-  '''
+      :param string: string with brackets
+      :return: boolean validation result
+    '''
 
-  if not string:
-    return False
-  
-  BRACKETS_PAIRS = {
-    '{': '}',
-    '}': '{',
-    '(': ')',
-    ')': '(',
-    '[': ']',
-    ']': '[',
-  }
-  
-  stack = []
-  splitted_str = list(string)
-  
-  for bracket in splitted_str:
-    if not len(stack):
-      stack.append(bracket)
-    else:
-      upper_stacked = stack[len(stack)-1]
-      if upper_stacked == BRACKETS_PAIRS[bracket]:
-        stack.pop()
-      else:
-        if stack[len(stack) -2] == BRACKETS_PAIRS[bracket]: 
-          # optimize loop: in case single bracket is inside valid ones
-          return False
-        stack.append(bracket)
+    if not string:
+        return False
 
-  return not bool(len(stack))     
-      
+    BRACKETS_PAIRS = {
+        '{': '}',
+        '}': '{',
+        '(': ')',
+        ')': '(',
+        '[': ']',
+        ']': '[',
+    }
+
+    stack = []
+    splitted_str = list(string)
+
+    for bracket in splitted_str:
+        if not len(stack):
+            stack.append(bracket)
+        else:
+            upper_stacked = stack[len(stack)-1]
+            if upper_stacked == BRACKETS_PAIRS[bracket]:
+                stack.pop()
+            else:
+                if stack[len(stack)-2] == BRACKETS_PAIRS[bracket]:
+                    # optimize loop: in case single bracket is inside valid ones
+                    return False
+                stack.append(bracket)
+
+    return not bool(len(stack))
+
 
 # Strings for validations
 brackets_str_list = [
-  "(){}",
-  "(){[{{(}}](){}}",
-  "{([])}",
-  "{{}}({[{)]",
-  "([{}])()[",
-  "(",
-  "{}(",
-  "",
+    "(){}",
+    "(){[{{(}}](){}}",
+    "{([])}",
+    "{{}}({[{)]",
+    "([{}])()[",
+    "(",
+    "{}(",
+    "",
 ]
 
 # tests
 validated_strings_list = list(
-  map(
-    lambda str: {"string": str, "isValid": validate_bracket_str(str)}, 
-    brackets_str_list
-  )
+    map(
+        lambda str: {"string": str, "isValid": validate_bracket_str(str)},
+        brackets_str_list
+    )
 )
 print(validated_strings_list)
