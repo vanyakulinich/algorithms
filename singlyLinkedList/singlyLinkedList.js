5fe71ea47601c2e091ad64ad2a05cc8f48ac497d
vanyakulinich <kulinichvanya@gmail.com>
2021-04-17 21:14:50 +0300

    feat(singlyLinkedList-js): singlyLinkedList list node class addded

---
+++ singlyLinkedList/singlyLinkedList.js
@@ -0,0 +1,10 @@
+/**
+ * List Node class
+ * stored the data and pointer to next element
+ */
+ class ListNode {
+  constructor(data = null, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
